73f0f90c630e70274b7e1b78c640ed333c0f3081
kimintrest <dianashebu@gmail.com>
2024-11-09 09:33:39 +0300

    Initial Menu

--- app/src/main/java/com/splitsquad/splitpay/EventFragment.kt
+++ app/src/main/java/com/splitsquad/splitpay/EventFragment.kt
@@ -7,15 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 
 
+class EventFragment : Fragment() {
 
-
-class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_event, container, false)
     }
+
 }
--- app/src/main/java/com/splitsquad/splitpay/MainActivity.kt
+++ app/src/main/java/com/splitsquad/splitpay/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.splitsquad.splitpay
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
