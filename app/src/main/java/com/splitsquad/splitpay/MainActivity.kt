c4dd9b85741299df5524801f7089e53bc60006a3
kimintrest <dianashebu@gmail.com>
2024-11-09 10:45:52 +0300

    End Menu

--- app/src/main/java/com/splitsquad/splitpay/MainActivity.kt
+++ app/src/main/java/com/splitsquad/splitpay/MainActivity.kt
@@ -3,29 +3,72 @@ package com.splitsquad.splitpay
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout drawerLayout;
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val  navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener (this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.email, R.string.email)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
 
         }
-    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        when(item.itemId){
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            R.id.nav_event -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, EventFragment()).commit()
+            R.id.nav_group -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, GroupFragment()).commit()
+            R.id.nav_search -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SearchFragment()).commit()
+            R.id.nav_bill -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, BillFragment()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
+
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
